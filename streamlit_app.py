8ad2fae0177bc25fc44621e6f989ab33855f37cc
ict <ict@nfinit.systems>
2023-12-04 11:47:50 -0700

    DABW3: Pre-populate pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') # set the index so multiselect will show fruit names
 
-streamlit.title('My Parents\' New Healthy Diner')
+streamlit.title('New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -14,6 +14,6 @@ streamlit.text('Avocado Toast')
 
 streamlit.header('Build Your Own Fruit Smoothie')
 # allow user to pick fruits from the list using a streamlit multi-select
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Strawberries','Avocado'])
 # show complete fruit table
 streamlit.dataframe(my_fruit_list)
