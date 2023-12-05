9ed6d33e104063e8a3b44cc9dfeec18fd3e5ad9e
ict <ict@nfinit.systems>
2023-12-04 19:18:13 -0700

    Add end user fruit submission

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,14 @@ if streamlit.button('Get fruits now'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+
+def insert_fruit_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('" + new_fruit + "')")
+        return "Added " + new_fruit
+
+add_new_fruit = streamlit.text_input('Submit a new fruit')
+if (streamlit.button('Add fruit to list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    f_result = insert_fruit_snowflake(add_new_fruit)
+    streamlit.text(f_result)
