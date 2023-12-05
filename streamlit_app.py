d28bff8742cc75d00e45b86f82a9d7c4de1c3e5c
ict <ict@nfinit.systems>
2023-12-04 17:02:31 -0700

    Typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruit Advice from Fruityvice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response.json())
+streamlit.text(fruityvice_response.json())
