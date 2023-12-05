ac63976c34deae4a7c8a77868d7c763a4c7b3074
ict <ict@nfinit.systems>
2023-12-04 19:11:42 -0700

    Attempt code fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,11 @@ except URLError as e:
 streamlit.header('Local fruits')
 
 def get_fruit_load_list():
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     with my_cnx.cursor as my_cur:
         my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
         return my_cur.fetchall()
 
 if streamlit.button('Get fruits now'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
